7651d7907a86af2fc51f10f4163da301d0341cf2
Ajay Pradhan <ajay.pradhan@metacube.com>
2022-09-29 12:20:49 +0530

    output

---
+++ Migrations/inititalData.sql
@@ -0,0 +1,8 @@
+insert into Categories values('Storage Device'), ('Electronics'), ('Sports'), ('Clothing');
+
+insert into Products values 
+('Pendrive', 1),
+('Hard Disk',1),
+('Refrigerator', 2),
+('Air Conditioner', 2),
+('TShirts', 4);
